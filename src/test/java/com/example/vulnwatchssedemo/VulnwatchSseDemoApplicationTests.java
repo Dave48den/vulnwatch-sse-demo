06825e1c4ea3fb5b0e0cca6f77b318ca8138687d
King David <chukwukakingdavid5@gmail.com>
2026-05-11 22:35:03 +0000

    Implement SSE real-time scan status updates

---
+++ src/test/java/com/example/vulnwatchssedemo/VulnwatchSseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vulnwatchssedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnwatchSseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
